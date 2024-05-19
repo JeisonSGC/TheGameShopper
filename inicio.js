ce83d83b2f671a78c3ee3656ef5a4fac766329b9
JeisonSGC <127995716+JeisonSGC@users.noreply.github.com>
2024-05-19 15:27:14 -0400

    Se agrega API de google maps

--- inicio.js
+++ inicio.js
@@ -12,3 +12,26 @@ function mostrarPaginaPrincipal() {
 
 
 window.addEventListener('load', mostrarPaginaPrincipal);
+
+
+  $('#map-icon').click(function() {
+    $('#dialog-box').toggle();
+  });
+
+  $('.close').click(function() {
+    $('#dialog-box').hide();
+  });
+
+
+
+  function iniciarMap(){
+    var coord = {lat:-33.5211 ,lng: -70.5806};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
